519e15cc37cff662f948564bc137b5f59d3aa976
Masha <mashushvik.2001@gmail.com>
2019-04-01 22:54:59 +0300

    math expression calc

--- main.php
+++ main.php
@@ -5,5 +5,4 @@ include_once 'MathExpression.php';
 
 $string = readline('Enter  mathematical expression: ');
 $exp = new MathExpression();
-echo $exp->get_expression($string);
 $exp::calculate($string);
